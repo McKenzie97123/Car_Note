501d797a3323a6d4e8d23a3e72052bdf0df49f4e
micha <michalkubera14@gmail.com>
2024-03-05 00:35:47 +0100

    Refactor Register.java

--- app/src/main/java/com/example/car_note/Register.java
+++ app/src/main/java/com/example/car_note/Register.java
@@ -40,37 +40,13 @@ public class Register extends AppCompatActivity {
             String userPassword = password.getText().toString();
             String userRepeatPassword = repeatPassword.getText().toString();
 
-            if (userEmail.isEmpty() || userName.isEmpty() || userLastName.isEmpty()
-                    || userPassword.isEmpty() || userRepeatPassword.isEmpty()){
-                Toast.makeText(this, "To register new user you need to fill " +
-                        "up all presented text fields", Toast.LENGTH_LONG).show();
-            }
-
-            if (validator.emailValidate(userEmail)) {
-                if (!validator.passwordValidate(userPassword)) {
-                    Toast.makeText(this, "Passwords needs to have minimum 8 chars" +
-                            ", and one Uppercase char", Toast.LENGTH_LONG).show();
-                    return;
-                }
-
-                if (!userPassword.equals(userRepeatPassword)) {
-                    Toast.makeText(this, "Passwords are different", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                String hashedUserPassword = passwordHasher.hashPassword(userPassword);
-                User user = new User(userEmail, userName, userLastName, hashedUserPassword);
-                System.out.println(user);
-                userRegister(user);
-            } else {
-                Toast.makeText(this, "To register new user you need to fill" +
-                        " up this form with valid email", Toast.LENGTH_LONG).show();
-            }
+           registerUser(userEmail, userName, userLastName, userPassword, userRepeatPassword);
         });
 
         login.setOnClickListener(v -> loginLayout());
     }
 
-    private void userRegister(User user) {
+    private void insertUser(User user) {
         try {
             dbUser.insertUser(user);
 
@@ -81,7 +57,43 @@ public class Register extends AppCompatActivity {
         }
     }
 
-    private void loginLayout(){
+    private void registerUser(
+            String email,
+            String name,
+            String lastName,
+            String password,
+            String repeatedPassword
+    ) {
+        if (email.isEmpty() || name.isEmpty() || lastName.isEmpty()
+                || password.isEmpty() || repeatedPassword.isEmpty()) {
+            Toast.makeText(this, "To register new user you need to fill " +
+                    "up all presented text fields", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (validator.emailValidate(email)) {
+            Toast.makeText(this, "To register new user you need to fill" +
+                    " up this form with valid email", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (!validator.passwordValidate(password)) {
+            Toast.makeText(this, "Passwords needs to have minimum 8 chars" +
+                    ", and one Uppercase char", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (!password.equals(repeatedPassword)) {
+            Toast.makeText(this, "Passwords are different", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        String hashedUserPassword = passwordHasher.hashPassword(password);
+        User user = new User(email, name, lastName, hashedUserPassword);
+        insertUser(user);
+    }
+
+    private void loginLayout() {
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(intent);
     }
